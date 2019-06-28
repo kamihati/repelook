62c80d14fe98a4d98cfea44a0261aec599b1c604
尹潇深 <1265466@qq.com>
2019-06-28 20:20:14 +0800

    连连看的基础代码。缺少gui部分

---
+++ point.py
@@ -0,0 +1,17 @@
+# coidng=utf8
+
+
+class Point:
+    """
+    连连看中每个图片的位置对象
+    """
+    def __init__(self, x, y):
+        """
+        初始化位置对象
+        :param x: 列
+        :param y: 行
+        """
+        self.x = x
+        self.y = y
+
+
